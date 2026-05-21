4e6310dee623344c477e9da61d8e756120a11f6a
npv34 <phanvanluan611996@gmail.com>
2026-05-21 19:31:54 +0700

    init project

---
+++ src/main/java/com/codegym/mathclass/MathClassApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.mathclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathClassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MathClassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codegym/mathclass/MathClassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.mathclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathClassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
