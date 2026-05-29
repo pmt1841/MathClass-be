ce1deb73270086c27c34f2ade9cfdde0d4cd9ee1
pmt1841 <phamminhtien1841@gmail.com>
2026-05-29 14:49:23 +0700

    feat: implement classroom creation and retrieval services with unit tests

--- src/main/java/com/codegym/mathclass/classroom/service/ClassroomService.java
+++ src/main/java/com/codegym/mathclass/classroom/service/ClassroomService.java
@@ -1,8 +1,12 @@
 package com.codegym.mathclass.classroom.service;
 
+import java.util.List;
+
 import com.codegym.mathclass.classroom.dto.ClassroomResponse;
 import com.codegym.mathclass.classroom.dto.CreateClassroomRequest;
 
 public interface ClassroomService {
     ClassroomResponse createClassroom(CreateClassroomRequest request, String currentUserEmail);
+
+    List<ClassroomResponse> getClassroomsByTeacher(String teacherEmail);
 }
