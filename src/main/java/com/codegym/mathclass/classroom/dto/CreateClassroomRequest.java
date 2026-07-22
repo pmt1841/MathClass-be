a7868712347af5cd72a346b995712be5de5fe622
NguyenHoang1122 <nguyenhoang9xbn@gmail.com>
2026-07-22 15:35:33 +0700

    fix missing import

--- src/main/java/com/codegym/mathclass/classroom/dto/CreateClassroomRequest.java
+++ src/main/java/com/codegym/mathclass/classroom/dto/CreateClassroomRequest.java
@@ -1,5 +1,6 @@
 package com.codegym.mathclass.classroom.dto;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,7 +13,7 @@ public class CreateClassroomRequest {
     @NotBlank(message = "Tên lớp không được để trống")
     private String name;
 
-    
+    @Min(value = 1, message = "Số học sinh tối đa phải lớn hơn 0")
     private Integer maxStudents;
 
     private String description;
